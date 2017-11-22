84f9519c982998d4513c8f8211ccc9b51756f03b
赵政 <www.a821400729@qq.com>
2017-11-22 14:53:27 +0800

    step2

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# webdevelop-todolist
+﻿# webdevelop-todolist
 flask实现一个todolist
 
 ## 从零到1实现一个flask的todolist
 （把实现的的每一步都写保存下来，可以更好的参考）
 
 step1 ： 使用wtf实现了一个登录的表单
+step2 ： 使用flask_alchemy实现的查询todolist
---
+++ step2/app.py
@@ -0,0 +1,47 @@
+"""
+step 1 : 实现一个登录的页面
+关键点：  使用wtf from 实现登录的表单
+
+step 2 ：使用sqlalchemy 实现了基本的查询todolist里面的内容
+关键点：
+1. 如果不熟悉orm，可能要先看一下sqlalchemy
+2. 然后看一下flask-sqlalchemy 这个东西 ，熟悉一下基本的语法
+"""
+from flask import (Flask, render_template, redirect, url_for, request, flash)
+import forms
+import config
+
+from ext import db
+from models import TodoList, User
+
+
+app = Flask(__name__)
+app.secret_key=config.SECRET_KEY
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://root:root@127.0.0.1/test"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db.init_app(app)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        form = forms.LoginFrom()
+        return render_template("login.html",form=form)
+    else:
+        username=request.form['username']
+        password=request.form['password']
+        return "<h1>home post method </h1> <h2> post data is {} and {}".format(username,password)
+
+
+@app.route('/show', methods=['GET', 'POST'])
+def showtodolist():
+    if request.method=="GET":
+        todolists =TodoList.query.all()
+        return render_template("index.html",todolists=todolists)
+    else:
+        return "<h1> method post </h1>"
+
+if __name__=="__main__":
+    app.run(debug=True,port=5002)
